9cc85509e039d03198baef17184c4c03986bd8b9
Ailton Borges de Lima Filho <ailton.filho@academico.ifrn.edu.br>
2023-04-17 14:32:03 -0300

    carlos

---
+++ script.js
@@ -0,0 +1,9 @@
+function startplay(){
+    let audio = document.querySelector("#audio")
+    audio.play()
+}
+
+function startplay2(){
+    let audio = document.querySelector("#audio2")
+    audio.play()
+}
